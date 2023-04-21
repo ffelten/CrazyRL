477e5928662dfa1d66ed2a316d25abcc72a31c65
ColineLedez <crocoline74@gmail.com>
2023-04-21 13:16:19 +0200

    fixed mistake

--- crazy_rl/test/test_algos.py
+++ crazy_rl/test/test_algos.py
@@ -50,7 +50,7 @@ def test_escort():
     parallel_api_test(
         Escort(
             drone_ids=np.array([0, 1, 2, 3]),
-            render_mode="human",
+            render_mode=None,
             init_flying_pos=np.array([[0, 0, 1], [1, 1, 1], [0, 1, 1], [2, 2, 1]]),
             init_target_location=np.array([1, 1, 2.5]),
             final_target_location=np.array([-2, -2, 3]),
